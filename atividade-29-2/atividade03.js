03efd315457207e35ebf6dd72cd8acaf2e6b0127
Darold08 <liviaaadarold20@gmail.com>
2024-02-29 11:43:23 -0400

    atividades

---
+++ atividade-29-2/atividade03.js
@@ -0,0 +1,15 @@
+class Imovel{
+    constructor(quartos) {
+        this.quartos = quartos;
+    }
+    constructor(tipo) {
+        this.tipo = tipo;
+    }
+     constructor(endereco) {
+        this.endereco = endereco;
+     }
+
+     exibirInformacoes() {
+        retur
+     }
+}
